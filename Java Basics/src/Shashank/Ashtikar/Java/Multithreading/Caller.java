7ef0157d3ec3cf8c9748f57a3bb1b34e17f3de96
unknown <coolshashank1@gmail.com>
2014-07-07 23:16:44 -0400

    Minor changes in the writer and the caller

--- Java Basics/src/Shashank/Ashtikar/Java/Multithreading/Caller.java
+++ Java Basics/src/Shashank/Ashtikar/Java/Multithreading/Caller.java
@@ -2,11 +2,11 @@ package Shashank.Ashtikar.Java.Multithreading;
 
 public class Caller {
 
-	public static void main(String[] args)
-	{
-		UnixPipe up = new UnixPipe(100);
-		System.out.println("Starting the threads");
-		new Thread(new Reader(up)).start();
-		new Thread(new Writer(up)).start();
-	}
+    public static void main(String[] args)
+    {
+        UnixPipe up = new UnixPipe(100);
+        System.out.println("Starting the threads");
+        new Thread(new Reader(up)).start();
+        new Thread(new Writer(up)).start();
+    }
 }
